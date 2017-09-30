44aa69930b861188cff384d76647546c4ccb2731
Michiel de Jong <michiel@ripple.com>
2017-09-30 20:59:44 +0200

    make a start with testnet-shop, which uses ilp-plugin-btp-client to connect to amundsen

---
+++ testnet-shop.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const crypto = require('crypto')
+const Plugin = require('../../michielbdejong/ilp-plugin-btp-client')
+function base64url (buf) { return buf.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '') }
+
+let fulfillments = {}
+let letters = {}
+
+const plugin = new Plugin({
+  btpUri: 'btp+wss://fa9Eghohgeikaithihie:Iegah6faideiR9aenaax@oamundsen.michielbdejong.com/api/17q3',
+  btpVersion: 'btp/alpha'
+})
+
+plugin.connect().then(function () {
+  plugin.on('incoming_prepare', function (transfer) {
+    if (transfer.amount !== '10') {
+      plugin.rejectIncomingTransfer(transfer.id, {
+        code: 'F04',
+        name: 'Insufficient Destination Amount',
+        message: 'Please send exactly 10 drops, you sent ' + transfer.amount,
+        triggered_by: plugin.getAccount(),
+        triggered_at: new Date().toISOString(),
+        forwarded_by: [],
+        additional_info: {}
+      })
+    } else {
+      // the ledger will check if the fulfillment is correct and if it was submitted before the transfer's
+      // rollback timeout
+      plugin.fulfillCondition(transfer.id, fulfillments[transfer.executionCondition]).catch(function () {})
+    }
+  })
+
+  http.createServer(function (req, res) {
+    if (letters[req.url.substring(1)]) {
+      res.end('Your letter: ' + letters[req.url.substring(1)])
+    } else {
+      const secret = crypto.randomBytes(32)
+      const fulfillment = base64url(secret)
+      const condition = base64url(crypto.createHash('sha256').update(secret).digest())
+      const letter = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split('')[(Math.floor(Math.random() * 26))]
+      fulfillments[condition] = fulfillment
+      letters[fulfillment] = letter
+      console.log('Generated letter for visitor on ', req.url, { secret, fulfillment, condition, letter })
+      res.end('Please send an Interledger payment by running: node ./pay.js ' + plugin.getAccount() + ' 10 ' + condition)
+    }
+  }).listen(8000)
+})
