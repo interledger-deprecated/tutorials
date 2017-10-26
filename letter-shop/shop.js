2da5769f1fe8991ec103bf31ae7a3b1c5a5b0378
Adrian Hope-Bailie <adrian@hopebailie.com>
2017-10-26 15:35:28 +0200

    Fix file missed in last update (#9)

--- letter-shop/shop.js
+++ letter-shop/shop.js
@@ -3,6 +3,7 @@ const url = require('url')
 const crypto = require('crypto')
 const plugin = require('./plugins.js').xrp.Shop()
 function base64url (buf) { return buf.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '') }
+function sha256 (preimage) { return crypto.createHash('sha256').update(preimage).digest() }
 
 let fulfillments = {}
 let letters = {}
