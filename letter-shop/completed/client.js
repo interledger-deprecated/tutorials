10b8e863aeb9f594b0ebe53e1b6141680d2d3486
Michiel de Jong <michiel@ripple.com>
2017-11-06 16:43:31 +0100

    feat: prepare Letter Shop Bonus Step for #23

---
+++ letter-shop/completed/client.js
@@ -0,0 +1,45 @@
+const IlpPacket = require('ilp-packet')
+const plugin = require('./plugins.js').xrp.Customer()
+const uuid = require('uuid/v4')
+const fetch = require('node-fetch')
+
+function base64url (buf) {
+  return buf.toString('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+}
+
+plugin.connect().then(function () {
+  return fetch('http://localhost:8000/')
+}).then(function (res) {
+  const parts = res.headers.get('Pay').split(' ')
+  if (parts[0] === 'interledger-condition') {
+    const destinationAmount = parts[1]
+    const destinationAddress = parts[2]
+    const condition = parts[3]
+    return plugin.sendTransfer({
+      id: uuid(),
+      from: plugin.getAccount(),
+      to: destinationAddress,
+      ledger: plugin.getInfo().prefix,
+      expiresAt: new Date(new Date().getTime() + 1000000).toISOString(),
+      amount: destinationAmount,
+      executionCondition: condition,
+      ilp: base64url(IlpPacket.serializeIlpPayment({
+        account: destinationAddress,
+        amount: destinationAmount,
+        data: ''
+      }))
+    })
+  }
+})
+
+plugin.on('outgoing_fulfill', function (transferId, fulfillmentBase64) {
+  fetch('http://localhost:8000/' + fulfillmentBase64).then(function (res) {
+    return res.text()
+  }).then(function (body) {
+    console.log(body)
+    return plugin.disconnect()
+  }).then(function () {
+    process.exit()
+  })
+})
